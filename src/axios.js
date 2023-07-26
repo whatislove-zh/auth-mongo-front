332502dfc7ff6303af7bb58b6ccc4d4e65d46168
Vladislav <vladgm233223@gmail.com>
2023-07-27 02:42:32 +0300

    deploy

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:4444",
+  baseURL: "https://auth-mongo-back-production.up.railway.app",
 });
 
 instance.interceptors.request.use((config) => {
